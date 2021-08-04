b8518a1f7a1790ad0d4d50538f22c3cb927a0750
Fabian <lindgrenfabian@gmail.com>
2021-08-04 16:56:35 +0200

    add fruit template

---
+++ core/src/com/literallyfabian/lynn/Fruit.java
@@ -0,0 +1,15 @@
+package com.literallyfabian.lynn;
+
+public final class Fruit {
+    public float x;
+    public int delay;
+    public boolean catched = false;
+    public int hitsound = -1;
+    public boolean hyper = false;
+
+    public enum size {
+        FRUIT,
+        DROPLET,
+        BANANA
+    }
+}
