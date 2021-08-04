b9fff4642461f209838556e7f17547c1863817df
Fabian <lindgrenfabian@gmail.com>
2021-08-04 17:00:16 +0200

    add fruit constructor

--- core/src/com/literallyfabian/lynn/Fruit.java
+++ core/src/com/literallyfabian/lynn/Fruit.java
@@ -1,15 +1,24 @@
 package com.literallyfabian.lynn;
 
 public final class Fruit {
-    public float x;
+    public int x;
     public int delay;
     public boolean catched = false;
-    public int hitsound = -1;
-    public boolean hyper = false;
+    public int hitsound;
+    public boolean hyper;
+    public Size size;
 
-    public enum size {
+    public enum Size {
         FRUIT,
         DROPLET,
         BANANA
+    }
+
+    public Fruit(int x, int delay, Size size, int hitsound, boolean hyper) {
+        this.x = x;
+        this.delay = delay;
+        this.size = size;
+        this.hitsound = hitsound;
+        this.hyper = hyper;
     }
 }
