c8e0453f183bf74423518aaff0db14c4d8ba7d6b
Fabian <lindgrenfabian@gmail.com>
2021-08-04 15:37:49 +0200

    add icon

--- desktop/src/com/literallyfabian/lynn/desktop/DesktopLauncher.java
+++ desktop/src/com/literallyfabian/lynn/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.literallyfabian.lynn.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.literallyfabian.lynn.Lynn;
@@ -10,6 +11,9 @@ public class DesktopLauncher {
 		config.title = "Lynn";
 		config.width = 1920;
 		config.height = 1080;
+		config.addIcon("images/icon_128.png", Files.FileType.Internal);
+		config.addIcon("images/icon_32.png", Files.FileType.Internal);
+		config.addIcon("images/icon_16.png", Files.FileType.Internal);
 		new LwjglApplication(new Lynn(), config);
 	}
 }
